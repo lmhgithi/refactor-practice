bf46a0aa6b63c7710582219b870949277dbe4716
lmhgithi <lmhfox@foxmail.com>
2020-08-03 21:47:58 +0800

    [liminghao] refactor: refact Direction extract functions

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -1,41 +1,37 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Direction {
     private final char direction;
+    private static final Map<Character, List<Direction>> map = new HashMap<Character, List<Direction>>() {{
+        put('N', Arrays.asList(new Direction('W'), new Direction('E')));
+        put('S', Arrays.asList(new Direction('E'), new Direction('W')));
+        put('E', Arrays.asList(new Direction('N'), new Direction('S')));
+        put('W', Arrays.asList(new Direction('S'), new Direction('N')));
+    }};
 
     public Direction(char direction) {
         this.direction = direction;
     }
 
-    public Direction turnRight() {
-        switch (direction) {
-            case 'N':
-                return new Direction('E');
-            case 'S':
-                return new Direction('W');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
+    public Direction turnLeft() {
+        if (!map.containsKey(direction)) {
+            throw new IllegalArgumentException();
         }
+        return map.get(direction).get(0);
     }
 
-    public Direction turnLeft() {
-        switch (direction) {
-            case 'N':
-                return new Direction('W');
-            case 'S':
-                return new Direction('E');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
+    public Direction turnRight() {
+        if (!map.containsKey(direction)) {
+            throw new IllegalArgumentException();
         }
+        return map.get(direction).get(1);
     }
+
 
     @Override
     public boolean equals(Object o) {
